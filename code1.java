850cee17b0f767b2c7f6c70a11d4427f5a97b52f
fullstackjava082023 <141508327+fullstackjava082023@users.noreply.github.com>
2023-08-13 21:15:10 +0300

    Create code1.java

---
+++ code1.java
@@ -0,0 +1,80 @@
+public class Main {
+    public static void main(String[] args) {
+//        Create three variables :
+//        First variable will hold your name
+//        Second variable will hold your last name
+//        Third variable will hold your age
+//        Print your name family name and age using those three variables.
+//                Try to use one command of print.
+
+
+        //Solution
+
+
+//        String name = "Arya";
+//        String myLastName = "Stark";
+//        int age = 9;
+//
+//        System.out.println("My name is: " + name );
+//        System.out.println("and my surname is: " + myLastName);
+//        System.out.println("and my age is: " + age);
+//
+
+//////////////////Variables types////////////////////
+        //whole numbers
+//            byte smallNumber = 80;
+//            short shortNumber = 9000;
+//            int intNumber = 900004344; //whole numbers
+//            long longNumber = 4355553435454545467L;
+//          //not whole numbers fractional
+//            float fractional = 1.5f;
+//            double doubleNumber = 1212432141241.421412414141;
+//
+
+           //Casting
+//           byte x = 99;
+//           int y = x;
+
+//           double x = 2345645646464554.5456456;
+//           int y = (int)x;
+
+//            double z = 234233454435345354d;
+
+//            System.out.println(y);
+
+//        boolean isLightOn = true;
+//        boolean isLectorNameJohn = false;
+
+
+//        System.out.println(isLectorNameJohn);
+//
+           ///OPERATORS
+        //Assignment operator : "="
+        // Arithmetic operators : "+,-,/,*"
+        //Logic operator
+        // &&   and
+        // ||   or
+        // !   not
+
+//        boolean flag1 = true && true && false && true && true ;
+//
+//        boolean flag2 =  (false || true)  && !false;
+//        System.out.println(flag2);
+
+        //Comparison operators:
+        // ==, !=, > , < , >= , <=
+//        int x = 9;
+//        int y = 8;
+//        System.out.println(x >= y);
+
+//        Create variable of type int and insert there the number - how many years ago you finished the high school.
+//        Print true if the number is 10 or higher. (it will print true if you finished the school 10 or more years ago)
+//        Print false otherwise.
+//                Tip:
+//        Use comparison operator inside the print.
+          //int beforeSchool = 5;
+//          System.out.println(finishedSchool >= 10);
+
+
+   }
+}
